341f1c0976267d4e2ad271adfd40d9c3675cb372
RangerMauve <RangerMauve@hotmail.com>
2014-05-25 20:55:19 -0400

    Functional message sending

--- index.js
+++ index.js
@@ -20,36 +20,39 @@ app.use(bodyParser);
 
 app.get("/chat/room/:room", eventSource, function (req, res) {
 	var room = req.param("room");
-
+	console.log("User connecting to SSE for", room);
 	listen("room:message:" + room, relay_data);
 	req.socket.on("close", function () {
 		unlisten(room, relay_data);
 	});
 
 	function relay_data(data) {
-		req.sse(data);
+		res.sse(data);
 	}
 });
 
 app.post("/chat/room/:room/message", function (req, res, next) {
 	var room = req.param("room");
 	var data = req.param("data");
-	pub.publish(room, data);
+	console.log("User messaging", room, "with", data);
+	pub.publish("room:message:" + room, data);
 	res.json(200, {
 		message: "OK"
 	});
 });
 
 function listen(name, fn) {
-	if (events.listeners(name) === 0) {
+	if (events.listeners(name).length === 0) {
 		sub.subscribe(name);
+		console.log("Subscribing to", name);
 	}
 	events.on(name, fn);
 }
 
 function unlisten(name, fn) {
 	events.removeListener(name, fn);
-	if (events.listeners(name) === 0) {
+	if (events.listeners(name).length === 0) {
+		console.log("Unsubscribing to", name);
 		sub.unsubscribe(name);
 	}
 }
--- static/chat_scripts.js
+++ static/chat_scripts.js
@@ -8,9 +8,29 @@ var chat = {
 	current_text: "",
 	post_to_current: function () {
 		console.log("Posting", chat.current_text, "to", chat.current_room, "as", chat.name);
+		reqwest({
+			url: "/chat/room/" + chat.current_room + "/message",
+			method: "post",
+			data: {
+				data: chat.current_text
+			},
+			error: console.error.bind(console)
+		});
 	},
 	join_new: function () {
 		console.log("Joining new room", prompt("Enter room name"));
+	},
+	listen_to: function (room) {
+		if (!room) return;
+		console.log("Listening to SSE for", room);
+		var es = new ServerSentEvent("/chat/room/" + room);
+		es.on("message", function (data) {
+			chat.messages.push({
+				room: room,
+				text: data
+			});
+		});
+		es.on("error", console.error.bind(console));
 	}
 };
 
@@ -18,6 +38,7 @@ var chat = {
 	var name = localStorage.getItem("username");
 	while (!name)
 		name = prompt("Enter your username");
+	localStorage.setItem("username", name);
 	chat.name = name;
 	var room = "";
 	initRoomData();
@@ -27,6 +48,9 @@ var chat = {
 		chat.rooms.push({
 			name: "lobby",
 			users: [chat.name]
+		});
+		chat.rooms.forEach(function (room) {
+			chat.listen_to(room.name);
 		});
 	}
 	model.chat = chat;
